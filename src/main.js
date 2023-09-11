fced2f8bc214a947f0cf2adfdf97a39dee967ba8
chinazhoujunbo <760548276@qq.com>
2023-09-11 11:16:31 +0800

    add

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUi from 'element-ui'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUi)
 new Vue({
   router,
   store,
---
+++ src/views/login.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'login'
+})
+</script>
+
+<template>
+  <div>
+    login
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
